2de32c9d4c9aea2f539494742662d4056350ec80
MS-kise <cist.b220.m.sudou@gmail.com>
2025-12-02 20:27:27 +0900

    洗米時間計算式を変更

--- src/main/java/org/example/schoollunch2/domain/洗米時間計算式.java
+++ src/main/java/org/example/schoollunch2/domain/洗米時間計算式.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class 洗米時間計算式 {
     public 洗米時間 計算する(お米の量 お米の量, 今の水温 今の水温) {
         double 洗米時間;
-        洗米時間 = お米の量.getお米の量() * 0.13 *8 / 今の水温.get今の水温();
+        洗米時間 = お米の量.getお米の量() * 0.003 *8 / 今の水温.get今の水温();
         return new 洗米時間(洗米時間);
     }
 }
