574d25beb66fe6af055f70c5f71ba5bf2ae02e99
MS-kise <cist.b220.m.sudou@gmail.com>
2025-12-02 20:31:43 +0900

    注水量計算式を変更

--- src/main/java/org/example/schoollunch2/domain/注水量計算式.java
+++ src/main/java/org/example/schoollunch2/domain/注水量計算式.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class 注水量計算式 {
     public 注水量 比例計算する(お米の量 お米の量) {
         int 水量;
-        水量 = お米の量.getお米の量() * 100;
+        水量 = (int) Math.ceil(お米の量.getお米の量() * 1.3);
         return new 注水量(水量);
     }
 }
